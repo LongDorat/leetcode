f4ba481c41ccc257d3c93aa91d50353a1700a4b3
Long Dorat <longdorat@outlook.com>
2025-06-30 22:08:30 +0700

    refactor: restructure solution and unit test to implement ISolution interface

--- Problems/Template/Solution.cs
+++ Problems/Template/Solution.cs
@@ -1,12 +1,18 @@
 namespace ProblemName;
 
-public class Solution
+public interface ISolution
 {
     /// <summary>
-    /// Problem description and approach
-    /// Time Complexity: O(?)
-    /// Space Complexity: O(?)
+    /// Problem description
     /// </summary>
+    /// <param name="parameters">Parameters for the method</param>
+    /// <returns>Return type description</returns>
+    /// <remarks>Time Complexity: O(?), Space Complexity: O(?)</remarks>
+    ReturnType MethodName(Parameters parameters);
+}
+
+public class Solution
+{
     public ReturnType MethodName(Parameters parameters)
     {
         // Implementation here
--- Problems/Template/UnitTest.cs
+++ Problems/Template/UnitTest.cs
@@ -1,16 +1,30 @@
+using Microsoft.Extensions.DependencyInjection;
+
 namespace ProblemName;
 
 public class UnitTests
 {
+    private readonly ISolution _solution;
+
+    public UnitTest()
+    {
+        // Simple DI container setup
+        var services = new ServiceCollection();
+        services.AddTransient<ISolution, Solution>();
+
+        var serviceProvider = services.BuildServiceProvider();
+        _solution = serviceProvider.GetRequiredService<ISolution>();
+    }
+
     [Fact]
     public void TestCase1()
     {
         // Arrange
         var solution = new Solution();
-        
+
         // Act
         var result = solution.MethodName(input);
-        
+
         // Assert
         Assert.Equal(expected, result);
     }
