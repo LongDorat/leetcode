4b6fac7808678ec304948acd80bcc5b0db0efab7
LongDorat <86357894+LongDorat@users.noreply.github.com>
2025-06-30 21:45:08 +0700

    Fixed 1. Two Sum's workflows (#2)
    * style: fixed typo in the file name
    
    * fixed: changed the file name so it won't interferes with the program
    
    * fixed: changed the file name so it won't interferes with the program
    
    * fix: update project names and configurations in solution file
    
    * feat(Two Sum): add GitHub Actions workflow and implement unit tests
    
    * feat(Two Sum): moved back to workflows folder
    
    * fix: standardize quotes in workflow file
    
    * refactor: rename folder and files for consistency
    
    * fix: remove verbosity option from test command in workflow
    
    * fix: remove unnecessary --no-build option from test command

---
+++ Problems/00001-00100/00001-Two_Sum/Solution.cs
@@ -0,0 +1,54 @@
+namespace Two_Sum;
+
+public interface ISolution
+{
+    /// <summary>
+    /// Finds two numbers in the array that add up to target using brute force approach.
+    /// </summary>
+    /// <param name="num">Array of integers</param>
+    /// <param name="target">Target sum</param>
+    /// <returns>Indices of the two numbers that sum to target</returns>
+    /// <remarks>Time: O(n²), Space: O(1)</remarks>
+    int[] BruteForceSolution(int[] num, int target);
+    /// <summary>
+    /// Finds two numbers that add up to target using hash map for O(n) time complexity.
+    /// </summary>
+    /// <param name="num">Array of integers</param>
+    /// <param name="target">Target sum</param>
+    /// <returns>Indices of the two numbers that sum to target</returns>
+    /// <remarks>Time: O(n), Space: O(n)</remarks>
+    int[] HashMapSolution(int[] num, int target);
+}
+
+public class Solution : ISolution
+{
+    public int[] BruteForceSolution(int[] num, int target)
+    {
+        for (int i = 0; i < num.Length; i++)
+        {
+            for (int j = i + 1; j < num.Length; j++)
+            {
+                if (num[i] + num[j] == target)
+                {
+                    return [i, j];
+                }
+            }
+        }
+        return [];
+    }
+
+    public int[] HashMapSolution(int[] num, int target)
+    {
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < num.Length; i++)
+        {
+            int complement = target - num[i];
+            if (map.TryGetValue(complement, out int index))
+            {
+                return [index, i];
+            }
+            map[num[i]] = i;
+        }
+        return [];
+    }
+}
