d2e3c00337b1eb39de0fc52163669e3f746bc1f1
Long Dorat <longdorat@outlook.com>
2025-08-16 12:45:09 +0700

    feat: add detailed documentation for solution function
    Updated the function documentation to include parameter and return value descriptions, along with time and space complexity notes.

--- templates/cpp/Solutions.hpp
+++ templates/cpp/Solutions.hpp
@@ -4,6 +4,12 @@
 #include "IncludeLib.hpp"
 
 // Replace the SolutionName with ProblemName if there is only one solution
+/**
+ * @brief Problem description summary
+ * @param input Input parameter description
+ * @return Return value description
+ * @note Time Complexity: O(n) | Space Complexity: O(1)
+ */
 int SolutionName(int args);
 
 #endif
