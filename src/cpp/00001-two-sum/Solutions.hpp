c2c8528286ccdb47891f8f447bfd3a632c16e2bb
Long Dorat <longdorat@outlook.com>
2025-08-16 12:23:43 +0700

    feat: add initial implementation of two-sum problem

---
+++ src/cpp/00001-two-sum/Solutions.hpp
@@ -0,0 +1,9 @@
+#ifndef SOLUTIONS_HPP
+#define SOLUTIONS_HPP
+
+#include "IncludeLib.hpp"
+
+std::vector<int> BruteForce(std::vector<int> nums, int target);
+std::vector<int> HashMap(std::vector<int> nums, int target);
+
+#endif // SOLUTIONS_HPP
---
+++ src/cpp/00001-two-sum/Tests/BruteForceTests.cpp
@@ -0,0 +1,74 @@
+#include <gtest/gtest.h>
+#include "../Solutions.hpp"
+
+TEST(TwoSum_BruteForce, BasicCase)
+{
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    std::vector<int> result = BruteForce(nums, target);
+    std::vector<int> expected = {0, 1};
+    EXPECT_EQ(result, expected);
+}
+
+TEST(TwoSum_BruteForce, TargetAtEnd)
+{
+    std::vector<int> nums = {3, 2, 4};
+    int target = 6;
+    std::vector<int> result = BruteForce(nums, target);
+    std::vector<int> expected = {1, 2};
+    EXPECT_EQ(result, expected);
+}
+
+TEST(TwoSum_BruteForce, SameNumberTwice)
+{
+    std::vector<int> nums = {3, 3};
+    int target = 6;
+    std::vector<int> result = BruteForce(nums, target);
+    std::vector<int> expected = {0, 1};
+    EXPECT_EQ(result, expected);
+}
+
+TEST(TwoSum_BruteForce, NegativeNumbers)
+{
+    std::vector<int> nums = {-1, -2, -3, -4, -5};
+    int target = -8;
+    std::vector<int> result = BruteForce(nums, target);
+    std::vector<int> expected = {2, 4};
+    EXPECT_EQ(result, expected);
+}
+
+TEST(TwoSum_BruteForce, MixedNumbers)
+{
+    std::vector<int> nums = {-3, 4, 3, 90};
+    int target = 0;
+    std::vector<int> result = BruteForce(nums, target);
+    std::vector<int> expected = {0, 2};
+    EXPECT_EQ(result, expected);
+}
+
+TEST(TwoSum_BruteForce, EmptyArray)
+{
+    std::vector<int> nums = {};
+    int target = 0;
+    std::vector<int> result = BruteForce(nums, target);
+    std::vector<int> expected = {};
+    EXPECT_EQ(result, expected);
+}
+
+TEST(TwoSum_BruteForce, SingleElement)
+{
+    std::vector<int> nums = {5};
+    int target = 5;
+    std::vector<int> result = BruteForce(nums, target);
+    std::vector<int> expected = {};
+    EXPECT_EQ(result, expected);
+}
+
+TEST(TwoSum_BruteForce, NoSolution)
+{
+    std::vector<int> nums = {1, 2, 3, 4};
+    int target = 10;
+    std::vector<int> result = BruteForce(nums, target);
+    std::vector<int> expected = {};
+    EXPECT_EQ(result, expected);
+}
