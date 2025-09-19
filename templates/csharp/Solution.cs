dbc8e68ab5be624e5d27d316fa5d421465e28bc9
Long Dorat <longdorat@outlook.com>
2025-09-19 20:49:08 +0700

    feat(csharp): added template structure

---
+++ templates/csharp/Solution.cs
@@ -0,0 +1,14 @@
+namespace Template;
+
+public class Solution
+{
+    /// <summary>
+    /// Example method that doubles the input value.
+    /// </summary>
+    /// <param name="input">The input integer to be doubled.</param>
+    /// <returns>The doubled value.</returns>
+    public int ExampleMethod(int input)
+    {
+        return input * 2;
+    }
+}
